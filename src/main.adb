4a8ac987e35bd911b7bc078b4109fd0e2bf93596
Johan <johaa1993@gmail.com>
2016-03-14 15:16:30 +0100

    Cleaner

--- src/main.adb
+++ src/main.adb
@@ -1,61 +1,77 @@
 with Ada.Text_IO;
 with Ada.Float_Text_IO;
-with Ada.Integer_Text_IO;
-with Sortings;
+with Ada.Strings;
+with Ada.Strings.Fixed;
 
 procedure Main is
 
    generic
       type Element is private;
-      type Index is (<>);
-      type Vector is array (Index range <>) of Element;
-      with procedure Put (Item : Element);
-   procedure Generic_Put_Vector (Item : Vector);
-
-   procedure Generic_Put_Vector (Item : Vector) is
-      use Ada.Text_IO;
+   procedure Generic_Swap (A, B : in out Element);
+   procedure Generic_Swap (A, B : in out Element) is
+      Temp : constant Element := A;
    begin
-      for E of Item loop
-         Put (E);
+      A := B;
+      B := Temp;
+   end;
+
+   generic
+      type Index is (<>);
+      type Element is private;
+      type Vector is array (Index range <>) of Element;
+      with function "<" (Left, Right : Element) return Boolean is <>;
+      with function ">" (Left, Right : Element) return Boolean is <>;
+   procedure Generic_Quicksort (Item : in out Vector);
+   procedure Generic_Quicksort (Item : in out Vector) is
+      procedure Swap is new Generic_Swap (Element);
+      pragma Suppress (Overflow_Check);
+      pragma Suppress (Range_Check);
+      Pivot : Element;
+      Left : Index;
+      Right : Index;
+   begin
+      if Item'Length > 1 then
+         Pivot := Item (Item'Last); -- Choose the pivot element.
+         Left := Item'First;
+         Right := Item'Last;
+         while Left <= Right loop
+            while Item (Left) < Pivot loop
+               Left := Index'Succ (Left);
+            end loop;
+            while Item (Right) > Pivot loop
+               Right := Index'Pred (Right);
+            end loop;
+            if Left <= Right then
+               Swap (Item (Left), Item (Right));
+               Left := Index'Succ (Left);
+               Right := Index'Pred (Right);
+            end if;
+         end loop;
+         Generic_Quicksort (Item (Item'First .. Right));
+         Generic_Quicksort (Item (Left .. Item'Last));
+      end if;
+   end;
+
+   type Planet is (Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune, Pluto);
+   type Planet_Season_Array is array (Planet range <>) of Float;
+   procedure Sort is new Generic_Quicksort (Planet, Float, Planet_Season_Array);
+   procedure Put (Item : Planet_Season_Array) is
+      use Ada.Text_IO;
+      use Ada.Float_Text_IO;
+      use Ada.Strings.Fixed;
+   begin
+      for I in Item'Range loop
+         Put (Tail (I'Img, Planet'Width));
+         Put (Item (I), 2, 2, 0);
          New_Line;
       end loop;
+      New_Line;
    end;
 
-
-   type Natural_Float is new Float range 0.0 .. Float'Last;
-   type Planet is (Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune, Pluto);
-
-   procedure Put (Item : Natural_Float) is
-   begin
-      Ada.Float_Text_IO.Put (Float (Item), 3, 3, 0);
-   end;
-
-   procedure Put (Item : Integer) is
-   begin
-      Ada.Integer_Text_IO.Put (Item, 3);
-   end;
-
-   type Integer_Array is array (Positive range <>) of Integer;
-   type Planet_Distance_Array is array (Planet range <>) of Natural_Float;
-
-   procedure Sort is new Sortings.Generic_Quicksort (Natural_Float, Planet, Planet_Distance_Array);
-   procedure Sort is new Sortings.Generic_Quicksort (Integer, Positive, Integer_Array);
-   procedure Rosetta_Sort is new Sortings.Generic_Rosetta_Quicksort (Natural_Float, Planet, Planet_Distance_Array);
-   procedure Rosetta_Sort is new Sortings.Generic_Rosetta_Quicksort (Integer, Positive, Integer_Array);
-
-   procedure Put is new Generic_Put_Vector (Natural_Float, Planet, Planet_Distance_Array, Put);
-   procedure Put is new Generic_Put_Vector (Integer, Positive, Integer_Array, Put);
-
-   A : Integer_Array := (1, 3, 0, -3, 3, 5, 6, -4);
-   B : Planet_Distance_Array (Planet) := (0.39, 0.723, 1.0, 1.524, 5.203, 9.539, 19.18, 30.06, 39.53);
-
+   Item : Planet_Season_Array (Planet) := (0.8, 0.1, 0.4, 0.9, 0.0, 0.7, 0.5, 0.3, 0.7);
 
 begin
-
-   Sort (A (2 .. 6));
-   Put (A);
-   Sort (B (Earth .. Pluto));
-   Put (B);
-
-   null;
+   Put (Item);
+   Sort (Item);
+   Put (Item);
 end;
